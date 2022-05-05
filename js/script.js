9305ef69da3b9b430a32662f5119877cfccbde52
zoeestewart <zuluoscarecho@gmail.com>
2022-05-05 10:16:30 +0300

    Added repos api

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const overview = document.querySelector(".overview");
 const username = "zululisbe";
+const repoList = document.querySelector(".repo-list");
 
 const gitUserInfo = async function () {
 	const userInfo = await fetch (`https://api.github.com/users/${username}`);
@@ -22,6 +23,22 @@ const displayUserInfo = function (data) {
 		<p><strong>Location:</strong> ${data.location}</p>
 		<p><strong>Number of public repos:</strong> ${data.public_repos}</p>
 	</div>
-	;`
+	`;
 	overview.append(div);
+	gitRepos();
+};
+
+const gitRepos = async function () {
+	const fetchRepos = await fetch (`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+	const repoData = await fetchRepos.json();
+	displayRepos(repoData);
+};
+
+const displayRepos = function (repos) {
+	for (const repo of repos) {
+		const repoItem = document.createElement("li");
+		repoItem.classList.add("repo");
+		repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+		repoList.append(repoItem);
+	}
 };
